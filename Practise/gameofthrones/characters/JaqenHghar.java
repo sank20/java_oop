b86615090b64ad70d811ccb2e8b207e083e03853
sankp <sanket@grayfos.com>
2018-10-05 21:19:38 +0530

    Fixes #2 added Jaqen H'ghar character

---
+++ Practise/gameofthrones/characters/JaqenHghar.java
@@ -0,0 +1,11 @@
+public class JaqenHghar extends Character{
+
+	 private List<String> titles = new ArrayList<String>();
+	    
+		public JaquenHghar(){
+			titles.add("Faceless Man");
+			nickname = "A Man has no name";
+			portrayedBy = "Tom Wlaschiha";
+			isAlive=true;
+		}
+}
